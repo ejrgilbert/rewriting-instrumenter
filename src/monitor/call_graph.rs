4ec16b88755e0b0d832852c4bf9d16d62a9cd1da
Elizabeth Gilbert <evgilber@andrew.cmu.edu>
2025-07-18 09:47:01 -0400

    implement the call-graph monitor

--- src/monitor/call_graph.rs
+++ src/monitor/call_graph.rs
@@ -1,7 +1,7 @@
-use crate::monitor::{call_flush_on_exit, import_lib_func, LocalsTracker, MemTracker};
-use wasmparser::{MemArg, Operator};
+use crate::monitor::{add_global, call_flush_on_exit, import_lib_func, LocalsTracker};
+use wasmparser::Operator;
 use wirm::ir::function::FunctionBuilder;
-use wirm::ir::id::{FunctionID, LocalID};
+use wirm::ir::id::{FunctionID, GlobalID};
 use wirm::ir::types::BlockType;
 use wirm::iterator::iterator_trait::{IteratingInstrumenter, Iterator};
 use wirm::iterator::module_iterator::ModuleIterator;
@@ -10,34 +10,25 @@ use wirm::DataType::I32;
 use wirm::{Location, Module, Opcode};
 
 pub fn instrument(mut wasm: Module) -> Module {
-    // setup mem tracker
-    let mut memory = MemTracker::new(vec![], &mut wasm);
     let mut locals = LocalsTracker::default();
-    let tracer = import_tracer_lib(&mut wasm);
+    let globals = Globals::new(&mut wasm);
+    let maps_lib = import_maps_lib(&mut wasm);
 
     let mut mod_it = ModuleIterator::new(&mut wasm, &vec![]);
-    let mut init_func = FunctionBuilder::new(&[], &[]);
-    inject_instrumentation(
-        &mut mod_it,
-        &mut init_func,
-        &mut memory,
-        &mut locals,
-        &tracer,
-    );
-    configure_init_func(init_func, &mut wasm);
-    flush(&tracer, &mut wasm);
+    inject_instrumentation(&mut mod_it, &mut locals, &globals, &maps_lib);
+    flush(&globals, &maps_lib, &mut wasm);
+    init_map_on_start(&globals, &maps_lib, &mut wasm);
 
     wasm
 }
 
 fn inject_instrumentation(
     wasm: &mut ModuleIterator,
-    init_func: &mut FunctionBuilder,
-    memory: &mut MemTracker,
     locals: &mut LocalsTracker,
-    tracer: &Tracer,
+    globals: &Globals,
+    maps: &Maps,
 ) {
-    let (mut curr_fid, curr_pc) = if let Location::Module {
+    let (mut curr_fid, ..) = if let Location::Module {
         func_idx,
         instr_idx,
         ..
@@ -56,22 +47,19 @@ fn inject_instrumentation(
                 curr_fid = func_idx;
 
                 // inject a function entry probe
-                wasm.func_entry();
-                anchor_probe(*curr_fid, curr_pc as u32, memory, tracer, init_func, wasm);
-                wasm.finish_instr();
+                func_entry_probe(*curr_fid, globals, maps, wasm);
             }
         } else {
             panic!("we don't support non-module locations (components don't work atm).")
         };
         if let Some(op) = wasm.curr_op() {
             match op {
-                Operator::Loop { .. } => {
-                    anchor_probe(*curr_fid, curr_pc as u32, memory, tracer, init_func, wasm)
-                }
-                Operator::BrIf { .. } | Operator::If { .. } => if_probe(locals, tracer, wasm),
-                Operator::BrTable { targets, .. } => {
-                    br_table_probe(targets.len(), locals, tracer, wasm)
-                }
+                Operator::Call { function_index }
+                | Operator::ReturnCall { function_index } => call_probe(*curr_fid, *function_index, globals, maps, wasm),
+                Operator::CallIndirect { .. }
+                | Operator::ReturnCallIndirect { .. }
+                | Operator::CallRef { .. }
+                | Operator::ReturnCallRef { .. } => call_indirect_probe(*curr_fid, globals, wasm),
                 _ => {}
             }
         }
@@ -82,174 +70,158 @@ fn inject_instrumentation(
     }
 }
 
-fn anchor_probe(
+fn func_entry_probe(
     fid: u32,
-    pc: u32,
-    memory: &mut MemTracker,
-    tracer: &Tracer,
-    init_func: &mut FunctionBuilder,
+    globals: &Globals,
+    maps: &Maps,
     wasm: &mut ModuleIterator,
 ) {
-    wasm.before();
+    wasm.func_entry();
 
-    let (alloc_id, memarg) = alloc_anchor_id(fid, pc, memory, tracer, init_func);
-
-    // now the actual probe logic
     wasm
-        // load the alloc_id
-        .u32_const(alloc_id)
-        .i32_load(memarg)
-        // call the tracer with the id
-        .call(tracer.on_anchor);
+        // check if we're trying to collect a call target
+        .global_get(globals.tracking_target)
+        .if_stmt(BlockType::Empty)
+            // the map ID
+            .global_get(globals.call_graph)
+            // (caller, fid)
+            .global_get(globals.caller)
+            .u32_const(fid)
+
+            // get the current value
+            .global_get(globals.call_graph)
+            // (caller, fid)
+            .global_get(globals.caller)
+            .u32_const(fid)
+            .call(maps.get)
+
+            // increment the current value by 1
+            .u32_const(1)
+            .i32_add()
+
+            .call(maps.insert)
+
+            // unset the tracking bool
+            .i32_const(0)
+            .global_set(globals.tracking_target)
+        .end();
+
+    wasm.finish_instr();
 }
 
-fn alloc_anchor_id(
+fn call_probe(
     fid: u32,
-    pc: u32,
-    memory: &mut MemTracker,
-    tracer: &Tracer,
-    init_func: &mut FunctionBuilder,
-) -> (u32, MemArg) {
-    let val_loc = memory.alloc_i32_var();
-
-    let mem = MemArg {
-        align: 0,
-        max_align: 0,
-        offset: 0, // this alloc var type has no header!
-        memory: memory.mem_id,
-    };
-
-    // logic to initialize the anchor_id for this probe.
-    init_func
-        // where to store
-        .u32_const(val_loc)
-        // get the anchor_id
+    target: u32,
+    globals: &Globals,
+    maps: &Maps,
+    wasm: &mut ModuleIterator
+) {
+    wasm.before();
+    wasm
+        // the map ID
+        .global_get(globals.call_graph)
+        // (caller, target)
         .u32_const(fid)
-        .u32_const(pc)
-        .call(tracer.init_anchor)
-        .i32_store(mem);
+        .u32_const(target)
 
-    (val_loc, mem)
+        // get the current value
+        .global_get(globals.call_graph)
+        // (caller, fid)
+        .u32_const(fid)
+        .u32_const(target)
+        .call(maps.get)
+
+        // increment the current value by 1
+        .u32_const(1)
+        .i32_add()
+
+        .call(maps.insert);
 }
 
-fn if_probe(locals: &mut LocalsTracker, tracer: &Tracer, wasm: &mut ModuleIterator) {
-    wasm.before();
-    let arg0 = bundle_args(wasm, locals);
-
-    wasm
-        // call the tracer with arg0
-        .local_get(arg0)
-        .call(tracer.on_if);
-
-    fix_stack(wasm, &vec![arg0]);
-    locals.reset_probe();
-}
-
-fn br_table_probe(
-    num_targets: u32,
-    locals: &mut LocalsTracker,
-    tracer: &Tracer,
-    wasm: &mut ModuleIterator,
+fn call_indirect_probe(
+    fid: u32,
+    globals: &Globals,
+    wasm: &mut ModuleIterator
 ) {
     wasm.before();
-    let arg0 = bundle_args(wasm, locals);
-
     wasm
-        // let target = arg0 <= (num_targets - 1) ? arg0 : num_targets
-        .local_get(arg0)
-        .u32_const(num_targets)
+        // set the tracking bool
         .i32_const(1)
-        .i32_sub()
-        .i32_lte_unsigned()
-        .if_stmt(BlockType::Type(I32))
-        .local_get(arg0)
-        .else_stmt()
-        .u32_const(num_targets)
-        .end()
-        // Call the tracer with this br_table's target
-        .call(tracer.on_br_table);
+        .global_set(globals.tracking_target)
 
-    fix_stack(wasm, &vec![arg0]);
-    locals.reset_probe();
+        .u32_const(fid)
+        .global_set(globals.caller);
 }
 
-/// Returns values in the order that they should be replaced on the stack!
-fn bundle_args(wasm: &mut ModuleIterator, locals: &mut LocalsTracker) -> LocalID {
-    let arg0 = LocalID(locals.use_local(I32, wasm));
-    wasm.local_set(arg0);
-    arg0
-}
-
-fn fix_stack(wasm: &mut ModuleIterator, orig_stack_vals: &Vec<LocalID>) {
-    for local in orig_stack_vals {
-        wasm.local_get(*local);
-    }
-}
-
-fn flush(tracer: &Tracer, wasm: &mut Module) {
-    let flush_fn = emit_flush_fn(tracer, wasm);
+fn flush(globals: &Globals, maps: &Maps, wasm: &mut Module) {
+    let flush_fn = emit_flush_fn(globals, maps, wasm);
     call_flush_on_exit(flush_fn, wasm)
 }
 
-fn emit_flush_fn(tracer: &Tracer, wasm: &mut Module) -> FunctionID {
+fn emit_flush_fn(globals: &Globals, maps: &Maps, wasm: &mut Module) -> FunctionID {
     let mut flush = FunctionBuilder::new(&[], &[]);
 
     // flush the map by calling the library
-    flush.call(tracer.flush);
+    flush.global_get(globals.call_graph).call(maps.flush);
 
     let flush_id = flush.finish_module(wasm);
     wasm.set_fn_name(flush_id, "flush_on_exit".to_string());
 
     flush_id
 }
-pub fn configure_init_func<'a>(init_func: FunctionBuilder<'a>, wasm: &mut Module<'a>) {
-    let state_init_id = if init_func.body.num_instructions > 0 {
-        // Call the probe init state function in the instr_init body
-        let state_init_id = init_func.finish_module(wasm);
-        wasm.set_fn_name(state_init_id, "init_probe_state".to_string());
-        Some(state_init_id)
-    } else {
-        None
-    };
-    call_instr_init_at_start(state_init_id, wasm);
+
+struct Globals {
+    call_graph: GlobalID,
+    tracking_target: GlobalID,
+    caller: GlobalID,
 }
-
-fn call_instr_init_at_start(state_init_id: Option<FunctionID>, module: &mut Module) {
-    if let Some(state_init_id) = state_init_id {
-        // now call `instr_init` in the module's start function
-        if let Some(start_fid) = module.start {
-            if let Some(mut start_func) = module.functions.get_fn_modifier(start_fid) {
-                start_func.func_entry();
-                start_func.call(state_init_id);
-                start_func.finish_instr();
-            } else {
-                unreachable!("Should have found the function in the module.")
-            }
-        } else {
-            // create the start function and call the `instr_init` function
-            let mut start_func = FunctionBuilder::new(&[], &[]);
-            start_func.call(state_init_id);
-
-            let start_fid = start_func.finish_module(module);
-            module.start = Some(start_fid);
+impl Globals {
+    pub fn new(wasm: &mut Module) -> Self {
+        Self {
+            call_graph: add_global(wasm),
+            tracking_target: add_global(wasm),
+            caller: add_global(wasm)
         }
     }
 }
 
-struct Tracer {
-    init_anchor: FunctionID,
-    on_anchor: FunctionID,
-    on_if: FunctionID,
-    on_br_table: FunctionID,
+fn init_map_on_start(globals: &Globals, maps: &Maps, wasm: &mut Module) {
+    // now call `instr_init` in the module's start function
+    if let Some(start_fid) = wasm.start {
+        if let Some(mut start_func) = wasm.functions.get_fn_modifier(start_fid) {
+            start_func.func_entry();
+            start_func.global_get(globals.call_graph).call(maps.create);
+            start_func.finish_instr();
+        } else {
+            unreachable!("Should have found the function in the module.")
+        }
+    } else {
+        // create the start function and call the `instr_init` function
+        let mut start_func = FunctionBuilder::new(&[], &[]);
+        start_func.global_get(globals.call_graph).call(maps.create);
+        let start_fid = start_func.finish_module(wasm);
+        wasm.start = Some(start_fid);
+    }
+}
+
+struct Maps {
+    create: FunctionID,
+    insert: FunctionID,
+    get: FunctionID,
     flush: FunctionID,
 }
-fn import_tracer_lib(wasm: &mut Module) -> Tracer {
-    let lib = "tracer";
-    Tracer {
-        init_anchor: import_lib_func(lib, "init_anchor", &[I32, I32], &[I32], wasm),
-        on_anchor: import_lib_func(lib, "on_anchor", &[I32], &[], wasm),
-        on_if: import_lib_func(lib, "on_if", &[I32], &[], wasm),
-        on_br_table: import_lib_func(lib, "on_br_table", &[I32], &[], wasm),
-        flush: import_lib_func(lib, "flush_csv", &[], &[], wasm),
+fn import_maps_lib(wasm: &mut Module) -> Maps {
+    let lib = "whamm_core";
+    Maps {
+        create: import_lib_func(lib, "create_tuple_i32_with_id", &[I32], &[], wasm),
+        insert: import_lib_func(
+            lib,
+            "insert_i32i32tuple_i32",
+            &[I32, I32, I32, I32],
+            &[],
+            wasm,
+        ),
+        get: import_lib_func(lib, "get_i32i32tuple_i32", &[I32, I32, I32], &[I32], wasm),
+        flush: import_lib_func(lib, "print_map_as_csv", &[I32], &[], wasm),
     }
 }
